0014e7ef20b3d497ac3f316591165edad51ab005
huanghuiqiang <1669903552@qq.com>
2022-09-09 16:01:57 +0800

    initial commit

---
+++ electron/main/index.ts
@@ -0,0 +1,83 @@
+import { app, BrowserWindow, shell, ipcMain } from 'electron'
+import { release } from 'os'
+import { join } from 'path'
+
+// Disable GPU Acceleration for Windows 7
+if (release().startsWith('6.1')) app.disableHardwareAcceleration()
+
+// Set application name for Windows 10+ notifications
+if (process.platform === 'win32') app.setAppUserModelId(app.getName())
+
+if (!app.requestSingleInstanceLock()) {
+  app.quit()
+  process.exit(0)
+}
+
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+
+export const ROOT_PATH = {
+  dist: join(__dirname, '../..'),
+  public: join(__dirname, app.isPackaged ? '../..' : '../../../public'),
+}
+
+let win: BrowserWindow | null = null
+const preload = join(__dirname, '../preload/index.js')
+const url = process.env.VITE_DEV_SERVER_URL
+const indexHtml = join(ROOT_PATH.dist, 'index.html')
+
+async function createWindow() {
+  win = new BrowserWindow({
+    title: 'Main window',
+    icon: join(ROOT_PATH.public, 'favicon.svg'),
+    webPreferences: {
+      preload,
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  if (app.isPackaged) {
+    win.loadFile(indexHtml)
+  } else {
+    win.loadURL(url)
+    win.webContents.openDevTools()
+  }
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  win = null
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('second-instance', () => {
+  if (win) {
+    if (win.isMinimized()) win.restore()
+    win.focus()
+  }
+})
+
+app.on('activate', () => {
+  const allWindows = BrowserWindow.getAllWindows()
+  if (allWindows.length) {
+    allWindows[0].focus()
+  } else {
+    createWindow()
+  }
+})
+
+
+ipcMain.handle('open-win', (event, arg) => {
+  const childWindow = new BrowserWindow({
+    webPreferences: {
+      preload,
+    },
+  })
+
+  if (app.isPackaged) {
+    childWindow.loadFile(indexHtml, { hash: arg })
+  } else {
+    childWindow.loadURL(`${url}/#${arg}`)
+  }
+})
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const App: React.FC = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <div>hello world</div>
+    </div>
+  )
+}
+
+export default App
